cb7ccde4a6e4f29a8085d2ae5827e9c286a57bf1
Sandeep <sandeep.s@applab.qa>
2025-05-15 10:05:57 +0530

    Splash screen created

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/homeScreen';
 import ProfileScreen from './src/screens/profileScreen';
 import { RootStackParamList } from './src/type';
+import SplashScreen from './src/screens/splashScreen';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -13,6 +14,7 @@ function App(): React.JSX.Element {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Splash" component={SplashScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Profile" component={ProfileScreen} />
       </Stack.Navigator>
